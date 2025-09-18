66e342b88f0817f9880c86cd0bb20b9abe361d13
Ingrid-FerreiraSilva <ingrid.pontes@aluno.ifsp.edu.br>
2025-09-17 22:21:33 -0300

    projeto1
    projeto1 dog

---
+++ projeto1.js
@@ -0,0 +1,68 @@
+//atual animação
+let playerState='idle';
+const dropdown=document.getElementById('animations');
+dropdown.addEventListener('change',function(e){
+    playerState=e.target.value;
+});
+
+
+//canvas para animações
+const canvas=document.getElementById('canvas1');
+const ctx=canvas.getContext('2d');
+const CANVAS_WIDTH=(canvas.width=600);
+const CANVAS_HEIGHT=(canvas.height=600);
+
+
+//Imagem
+const playerImage=new Image();
+playerImage.src='imagem/shadow_dog.png'
+const spriteWidth=575;
+const spriteHeight=523;
+let gameFrame=0;
+const staggerFrames=5;
+
+const spriteAnimations=[];
+
+const animationStates=[{name:'idle',frames: 7},{name:'pulo',frames: 7},{name:'cai',frames: 7},
+    {name:'corre',frames: 9},
+    {name:'stun',frames: 11},
+    {name:'senta',frames: 5},
+    {name:'rolar',frames: 7},
+    {name:'morde',frames: 7},
+    {name:'morte',frames: 12},
+    {name:'dano',frames: 4}];
+
+animationStates.forEach((state,index)=>{
+    let frames={
+        loc:[],
+    };
+    for (let j=0;j<state.frames;j++){
+        let positionX=j*spriteWidth;
+        let positionY=index*spriteHeight;
+        frames.loc.push({x:positionX,y:positionY,});
+    }
+    spriteAnimations[state.name]=frames;
+
+});
+
+function animate(){
+    ctx.clearRect(0,0,CANVAS_WIDTH,CANVAS_HEIGHT);
+    let position = Math.floor(gameFrame/staggerFrames) % spriteAnimations[playerState].loc.length;
+    let frameX= spriteWidth *position;
+    let frameY= spriteAnimations[playerState].loc[position].y;
+    
+    ctx.drawImage(
+        playerImage,
+        frameX,
+        frameY,
+        spriteWidth,
+        spriteHeight,
+        0,
+        0,
+        spriteWidth,
+        spriteHeight);
+
+    gameFrame++;
+    requestAnimationFrame(animate);
+}
+animate();
